631c254a5713c023394791584b1de8f7420a8e40
Tyler Butters <dapegral@gmail.com>
2016-04-21 00:47:13 -0400

    Graph scores via Highcharts
    Pulled in a very nice library that works well with the kind of data
    we're displaying, websocket tech, and provides an easy-to-use api
    to customize the looks of it.

--- static/assets/js/ws.js
+++ static/assets/js/ws.js
@@ -6,16 +6,26 @@ $(function() {
     }
     conn.onmessage = function(evt) {
         results = JSON.parse(evt.data);
-        appendScores(results)
+        appendScores(results);
+        updateChart(results);
     }
+});
+
 function appendScores(res) {
     res.forEach(function(r) {
-        var row = $('#result-list').find('#' + r.teamname);
+        var row = $("#result-list").find('#' + r.teamname);
         row.find('.teamnumber').html(r.teamnumber);
         row.find('.teamname').html(r.teamname);
         row.find('.points').html(r.points);
     });
 }
-
-});
-
+function updateChart(res) {
+    $("#hc_scoreboard").highcharts()
+        .get(":scores")
+        .setData(
+            // "scores" is an array of [{teamname}, {score}]
+            res.map( function(r) {
+                return [r.teamname, r.points];
+            })
+        );
+}
