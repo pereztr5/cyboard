12412e3919173d372dbae2c49be31c4635b0bc3c
Tyler Butters <dapegral@gmail.com>
2017-10-20 21:42:58 -0400

    Add score breakdown to web display
    The big scoreboard chart is now a stacked bar graph,
    representing points in the CTF and Services categories.
    Tooltip, label, and legend styles were tweaked to improve
    legibility and responsiveness.
    
    Also, fixed the spaces between scores where a comma would be.
    This spacing caused the point labels to look really 'off'.
    
    The table below the chart has been updated to include all
    of this same information.

--- static/assets/js/ws.js
+++ static/assets/js/ws.js
@@ -11,22 +11,40 @@ $(function() {
     };
 });
 
+function joinResultTypes(results) {
+    return results.reduce((scores, r) => {
+        const score = scores[r.teamname];
+        if(score) {
+            score[r.type.toLowerCase()] = r.points;
+            score.points = score.ctf + score.service;
+        } else {
+            scores[r.teamname] = {
+                teamname: r.teamname,
+                teamnumber: r.teamnumber,
+                [r.type.toLowerCase()]: r.points,
+            };
+        }
+        return scores;
+    }, {})
+}
+
 function appendScores(res) {
-    res.forEach(function(r) {
+    const scores = joinResultTypes(res);
+
+    Object.keys(scores).map(k => scores[k]).forEach(r => {
         var row = $('#result-list').find('#' + r.teamname);
         row.find('.teamnumber').html(r.teamnumber);
         row.find('.teamname').html(r.teamname);
         row.find('.points').html(r.points);
+        row.find('.service').html(r.service);
+        row.find('.ctf').html(r.ctf);
     });
 }
 
 function updateChart(res) {
-    $('#hc_scoreboard').highcharts()
-        .get(':scores')
-        .setData(
-            // "scores" is an array of [{teamname}, {score}]
-            res.map( function(r) {
-                return [r.teamname, r.points];
-            })
-        );
+    const chart = $('#hc_scoreboard').highcharts();
+    build_hc_series(res).forEach(series => {
+        chart.get(series.id).setData(series.data, false);
+    });
+    chart.redraw();
 }
