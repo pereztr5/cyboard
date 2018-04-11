a0855dcb822aeebcd84408e47f36b01fc6fe2462
Tyler Butters <dapegral@gmail.com>
2018-04-11 10:44:54 -0400

    📑 Comment the current hacky postgres stuff
    I tried to make tests as well, but there's a bit more stateful
    bootstrapping that I don't want to resolve right now, but I'll
    find a way to test the SQL some other month (famous last words).

--- server/postgres.go
+++ server/postgres.go
@@ -10,6 +10,19 @@ import (
 	"github.com/jackc/pgx/pgtype"
 )
 
+// PostgresMetricsResult represents the `cy.result` table schema
+// in the database.
+//
+// This table contains two similar but different `types`. The ChallengeName
+// column is only used for "type = 'CTF'" rows, whereas the ExitStatus column
+// is for "type = 'Service'" rows. There are better ways to implement this
+// (such as JSONB columns or actually competent table design), but that can be
+// fixed when there's more time.
+//
+// This type is not directly used at the moment, as most methods for insertion
+// in `github.com/jackc/pgx` take explicit interface{} slices. There is no
+// retrieval of rows from Postgres in cyboard, all those queries are done in
+// Grafana, a data analysis tool. The type signature sits here for reference.
 type PostgresMetricsResult struct {
 	Timestamp     time.Time   `sql:"timestamp"`
 	Teamname      string      `sql:"teamname"`
@@ -22,21 +35,30 @@ type PostgresMetricsResult struct {
 }
 
 const (
+	// pgStmtScoreCapturedFlag is the sole prepared statement, used to insert a scored flag into PG
 	pgStmtScoreCapturedFlag = "scoreCapturedFlag"
 )
 
 var (
+	// pgPool is the global conn for doing Postgres stuff.
 	pgPool *pgx.ConnPool
 
-	pgResultTable    = pgx.Identifier{"cy", "result"}
+	// pgResultTabe is the schema and table name for every teams' scores
+	pgResultTable = pgx.Identifier{"cy", "result"}
+	// pgServiceColumns are the names of the columns with values for "type = 'Service'" rows
 	pgServiceColumns = []string{
 		"timestamp", "teamname", "teamnumber", "points", "type", "category", "exit_status",
 	}
+	// pgServiceColumns are the names of the columns with values for "type = 'CTF'" rows
 	pgCtfColumns = []string{
 		"timestamp", "teamname", "teamnumber", "points", "type", "category", "challenge_name",
 	}
 )
 
+// SetupPostgres connects to a PG instance using the given `uri` parameter.
+// If the uri is empty, Postgres support is disabled. Otherwise, the global
+// `pgPool` will be set up, which will signal the app to duplicate scores into
+// Postgres as well as Mongodb.
 func SetupPostgres(uri string) {
 	// If the uri is unconfigured, skip saving data to Postgres
 	if uri == "" {
@@ -71,6 +93,7 @@ func SetupPostgres(uri string) {
 	}
 }
 
+// PostgresScoreCapturedFlag saves a single scored flag into postgres `cy.result` table
 func PostgresScoreCapturedFlag(res *Result) error {
 	if pgPool == nil {
 		return nil
@@ -81,6 +104,8 @@ func PostgresScoreCapturedFlag(res *Result) error {
 	return err
 }
 
+// PostgresScoreMany saves a slice of results into Postgres.
+// Either for 'CTF', or 'Service' type results.
 func PostgresScoreMany(results []Result, category string) error {
 	if pgPool == nil {
 		return nil
@@ -117,14 +142,18 @@ func PostgresScoreMany(results []Result, category string) error {
 	return err
 }
 
+// PostgresScoreServices saves service checker results into postgres
 func PostgresScoreServices(results []Result) error {
 	return PostgresScoreMany(results, Service)
 }
 
+// PostgresScoreServices saves bonus points/flags from results into postgres
 func PostgresScoreBonusFlags(results []Result) error {
 	return PostgresScoreMany(results, CTF)
 }
 
+// PgConfigAsString returns a string repr of a pgx.ConnConfig.
+// The password field is bleeted out if it is not-empty.
 func PgConfigAsString(c *pgx.ConnConfig) string {
 	var pw string
 	if c.Password != "" {
