39b36b91f8701145ea7255b0cf41a7a83371c1eb
Tyler Butters <dapegral@gmail.com>
2018-09-26 22:05:51 -0400

    Automatically sync service uptime display on clients
    The service uptime matrix (all the green/red/yellow circles on the
    scoreboard page) now handle when services are added/removed,
    automatically.
    
    Uses a bunch of jquery and a hand-written, flimsy DOM diffing algo,
    that every Js 201X framework does internally.
    
    If the number of teams changes, the Js running should detect this and it
    will just reload the whole page.
    
    Service add/delete can happen so suddenly that contestants might not
    notice, but if they're given a warning through other comm channels,
    they should be able to expect the change.

--- static/assets/js/serviceWs.js
+++ static/assets/js/serviceWs.js
@@ -4,16 +4,34 @@ $(function() {
     const conn = new WebSocket(endpoint);
 
     conn.onclose = (evt) => {
-        const $statusNode = $('#status');
-        $statusNode.textContent = 'Connection closed';
+        // reuse the top-left (i)nfo box, replace it with a warning message
+        const $errorNode = $('.sv-help .fa-info-circle')
+            .attr('class', 'sv-help-error text-warning')
+            .text('Live feed closed. Page should try to reload soon.');
     };
     conn.onmessage = (evt) => {
         results = JSON.parse(evt.data);
-        sync_services(results)
+        try {
+            syncServices(results)
+        } catch(e) {
+            if (e instanceof ErrorTeamSync) {
+                conn.close();
+                const sometimeInTenSecs = Math.floor(Math.random() * (10 * 1000));
+                console.warn(`Page reloading in ${sometimeInTenSecs}ms`);
+                window.setTimeout(() => window.location.reload(), sometimeInTenSecs);
+            } else {
+                conn.close();
+                console.error(e);
+            }
+        }
     };
 });
 
-function sync_services(data) {
+function ErrorTeamSync() {
+    this.message = 'teams out of sync';
+}
+
+function syncServices(data) {
     /* data looks like: [{
         "service_id":1,
         "service_name":"WWW Content",
@@ -21,26 +39,90 @@ function sync_services(data) {
      },
      {...}, ...]
     */
-    data.forEach(service => {
-        const $group = $('div').find(`[data-check="${service.service_id}"]`);
-        service.statuses.forEach((status, idx) => {
-            const $statusBox = $group.find(`[data-team=${idx}]`);
 
-            // If the status hasn't changed, skip past this box/node.
-            if($statusBox.attr('data-status') === status) {
-                return;
-            }
+    /* Diff the dom, apply changes */
 
-            let newIcon;
-            switch(status) {
-            case 'pass': newIcon = 'fa-arrow-circle-up text-success'; break;
-            case 'fail': newIcon = 'fa-arrow-circle-down text-danger blink'; break;
-            case 'partial': newIcon = 'fa-exclamation-circle text-warning'; break;
-            default:     newIcon = 'fa-question-circle text-muted'; break;
-            }
-            $statusBox.attr('class', `fa ${newIcon}`)
-                      .attr('data-status', status);
-        });
-    });
+    const $serviceDisplay = $('.service-statuses');
+    const $rows = $serviceDisplay.children('.sv-row');
+    const $teamRows = $rows.first().children('.sq-team');
+    const $serviceRows = $rows.slice(1);
+
+    // If the number of columns is out of sync because a team was added/disabled (unlikely),
+    // just reload the page by throwing up an exception.
+    if (data.length === 0 || data[0].statuses.length !== $teamRows.length) {
+        throw new ErrorTeamSync();
+    }
+
+    let i, j;
+    for (i=j=0; i < $serviceRows.length && j < data.length;) {
+        const {service_id, service_name, statuses} = data[j];
+
+        const $sRow = $($serviceRows[i]);
+        const domServiceID = $sRow.data('check');
+
+        if (domServiceID > service_id) {
+            // insert service row
+            const $newService = newServiceRow(service_id, service_name, statuses);
+            $sRow.before($newService);
+            j++;
+        } else if (domServiceID < service_id) {
+            // delete service row
+            $sRow.remove();
+            i++;
+        } else {
+            // update service row
+            updateServiceStatusBoxes($sRow, statuses);
+            i++; j++;
+        }
+    }
+
+    /* Handle trails in each array; At most one of these two loops will run. */
+
+    for (; i < $serviceRows.length; i++) {
+        const $sRow = $($serviceRows[i]);
+        $sRow.remove();
+    }
+
+    for (; j < data.length; j++) {
+        const {service_id, service_name, statuses} = data[j];
+        $serviceDisplay.append(
+            newServiceRow(service_id, service_name, statuses)
+        );
+    }
 }
 
+function newServiceRow(id, name, statuses) {
+    const $row = $(`<div class="sv-row"></div>`).attr('data-check', id);
+    $row.append( $(`<div class="sq sq-label sq-service"></div>`).text(name) );
+
+    const $boxes = statuses.map((_, idx) => {
+        const $ico = $(`<span class="fa" aria-hidden="true"></span>`).attr('data-status', '');
+        const $sq = $(`<div class="sq"></div>`).append($ico);
+        return $sq;
+    });
+    $row.append($boxes);
+    updateServiceStatusBoxes($row, statuses);
+    return $row;
+}
+
+function updateServiceStatusBoxes($serviceRow, statuses) {
+    const $statusBoxes = $serviceRow.find("span[data-status]");
+    statuses.forEach((status, idx) => {
+        const $statusBox = $($statusBoxes[idx]);
+
+        // If the status hasn't changed, skip past this box/node.
+        if($statusBox.attr('data-status') === status) {
+            return;
+        }
+
+        let newIcon;
+        switch(status) {
+        case 'pass': newIcon = 'fa-arrow-circle-up text-success'; break;
+        case 'fail': newIcon = 'fa-arrow-circle-down text-danger blink'; break;
+        case 'partial': newIcon = 'fa-exclamation-circle text-warning'; break;
+        default:     newIcon = 'fa-question-circle text-muted'; break;
+        }
+        $statusBox.attr('class', `fa ${newIcon}`)
+                  .attr('data-status', status);
+    });
+}
